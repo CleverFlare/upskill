08e83732a7ac4b0dd750bfeffc68f007ca8f44b5
flare <blastclever@gmail.com>
2023-12-25 00:06:46 +0200

    fix: temporary fix to the width of the navbar link buttons

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,12 +6,12 @@ import { Button } from "./ui/button";
 export default function Navbar({ className, ...props }: ComponentProps<"div">) {
   return (
     <div className="w-full flex justify-center py-4" {...props}>
-      <Container className={cn("flex justify-between w-full", className)}>
+      <Container className={cn("flex justify-between items-center w-full", className)}>
         <p className="text-2xl font-bold text-blue-500">UpSkill</p>
-        <div className="flex gap-3">
-          <Button variant="ghost" size="small">Home</Button>
-          <Button variant="ghost" size="small">About Us</Button>
-          <Button variant="ghost" size="small">Courses</Button>
+        <div className="flex gap-10 h-7">
+          <Button variant="ghost" size="small" className="px-2">Home</Button>
+          <Button variant="ghost" size="small" className="px-2">About Us</Button>
+          <Button variant="ghost" size="small" className="px-2">Courses</Button>
         </div>
         <div className="flex gap-3">
           <Button variant="outline" size="icon">B</Button>
