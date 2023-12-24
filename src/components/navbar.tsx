8ea2aae6d57c9730c274d500135401b45e5e034d
flare <blastclever@gmail.com>
2023-12-24 23:31:27 +0200

    chore: add size small to the navbar link buttons

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,9 +9,9 @@ export default function Navbar({ className, ...props }: ComponentProps<"div">) {
       <Container className={cn("flex justify-between w-full", className)}>
         <p className="text-2xl font-bold text-blue-500">UpSkill</p>
         <div className="flex gap-3">
-          <Button variant="ghost">Home</Button>
-          <Button variant="ghost">About Us</Button>
-          <Button variant="ghost">Courses</Button>
+          <Button variant="ghost" size="small">Home</Button>
+          <Button variant="ghost" size="small">About Us</Button>
+          <Button variant="ghost" size="small">Courses</Button>
         </div>
         <div className="flex gap-3">
           <Button variant="default" size="icon">B</Button>
