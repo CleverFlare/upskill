9f96a223f834b3df13cb6fa00a0cf533f0661e67
flare <blastclever@gmail.com>
2023-12-25 18:59:59 +0200

    fix: change size small to size sm in navbar link buttons

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -8,10 +8,10 @@ export default function Navbar({ className, ...props }: ComponentProps<"div">) {
     <div className="w-full flex justify-center py-4" {...props}>
       <Container className={cn("flex justify-between items-center w-full", className)}>
         <p className="text-2xl font-bold text-blue-500">UpSkill</p>
-        <div className="flex gap-10 h-7">
-          <Button variant="ghost" size="small" className="px-2">Home</Button>
-          <Button variant="ghost" size="small" className="px-2">About Us</Button>
-          <Button variant="ghost" size="small" className="px-2">Courses</Button>
+        <div className="flex gap-10">
+          <Button variant="ghost" size="sm">Home</Button>
+          <Button variant="ghost" size="sm">About Us</Button>
+          <Button variant="ghost" size="sm">Courses</Button>
         </div>
         <div className="flex gap-3">
           <Button variant="outline" size="icon">B</Button>
