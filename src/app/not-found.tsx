bd99e1fba8cb086f458d262d949919171602d7ab
flare <blastclever@gmail.com>
2023-12-25 20:26:46 +0200

    feat: add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import Container from "@/components/container"
+
+export default function NotFound() {
+  return (
+    <Container>
+      <h1 className="text-3xl font-bold"><span className="text-blue-600">404</span> - Not Found!</h1>
+    </Container>
+  )
+}
