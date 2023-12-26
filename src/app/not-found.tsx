e83ffac1014387878939a9ace3bc3851ce88592b
flare <blastclever@gmail.com>
2023-12-26 14:00:02 +0200

    fix: make the 404 message in the center of the screen

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,11 @@
-import Container from "@/components/container"
+import Container from "@/components/container";
 
 export default function NotFound() {
   return (
-    <Container>
-      <h1 className="text-3xl font-bold"><span className="text-blue-600">404</span> - Not Found!</h1>
+    <Container className="flex h-full items-center justify-center">
+      <h1 className="text-3xl font-bold">
+        <span className="text-blue-600">404</span> - Not Found!
+      </h1>
     </Container>
-  )
+  );
 }
