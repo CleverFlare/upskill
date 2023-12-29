a907543e6471f21f23f593cd0a20ba281f6bdea8
flare <blastclever@gmail.com>
2023-12-29 15:52:39 +0200

    fix: add as to all icon types the values

--- src/data/values.tsx
+++ src/data/values.tsx
@@ -8,7 +8,7 @@ import {
   HiSparkles,
   HiUserGroup,
 } from "react-icons/hi2";
-import type { IconType } from "react-icons";
+import { IconType } from "react-icons";
 
 type ValuesType = {
   headline: string;
@@ -19,32 +19,32 @@ type ValuesType = {
 export const values = atom<ValuesType[]>([
   {
     headline: "Transformation, not just training.",
-    Icon: HiCog6Tooth,
+    Icon: HiCog6Tooth as IconType,
     text: "We believe in empowering individuals to unlock their full potential through transformative learning experiences, not just rote memorization.",
   },
   {
     headline: "Adaptability in the face of change.",
-    Icon: HiPuzzlePiece,
+    Icon: HiPuzzlePiece as IconType,
     text: "We embrace the ever-evolving landscape of knowledge and equip learners with the skills and agility to thrive in a dynamic world.",
   },
   {
     headline: "Engaging learning, every step of the way.",
-    Icon: HiSparkles,
+    Icon: HiSparkles as IconType,
     text: "We prioritize curiosity and passion over dry lectures, making learning an adventure, not a chore.",
   },
   {
     headline: "Community that fuels growth.",
-    Icon: HiUserGroup,
+    Icon: HiUserGroup as IconType,
     text: "We foster a vibrant network of learners, mentors, and experts where collaboration and shared experiences accelerate individual progress.",
   },
   {
     headline: "Embrace mistakes, champion progress.",
-    Icon: HiArrowUp,
+    Icon: HiArrowUp as IconType,
     text: "We believe mistakes are stepping stones, not setbacks. We celebrate the journey of learning and encourage continuous improvement.",
   },
   {
     headline: "Passion fuels knowledge.",
-    Icon: HiFire,
+    Icon: HiFire as IconType,
     text: "We are driven by a shared passion for learning and believe in the transformative power of knowledge for individuals and communities.",
   },
 ]);
