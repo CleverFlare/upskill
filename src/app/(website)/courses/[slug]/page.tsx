a79bb2288723ce08e201988d98cfbd3a5aefb0f2
Flare <blastclever@gmail.com>
2024-04-01 13:59:19 +0200

    feat(course details): check for session and display the appropriate button

--- src/app/(website)/courses/[slug]/page.tsx
+++ src/app/(website)/courses/[slug]/page.tsx
@@ -3,12 +3,14 @@ import Container from "@/components/container";
 import { Button } from "@/components/ui/button";
 import { db } from "@/server/db";
 import type { Course } from "@prisma/client";
+import { getServerSession } from "next-auth";
 import Link from "next/link";
 import { redirect } from "next/navigation";
-import { HiArrowRightOnRectangle } from "react-icons/hi2";
+import { HiArrowLeftOnRectangle } from "react-icons/hi2";
 
 export default async function Page({ params }: { params: { slug: string } }) {
   let databaseCourseData: Course | null;
+  const session = await getServerSession();
 
   try {
     databaseCourseData = await db.course.findUnique({
@@ -31,12 +33,20 @@ export default async function Page({ params }: { params: { slug: string } }) {
         technologies={(databaseCourseData?.technologies as []) ?? []}
         prerequisites={(databaseCourseData?.prerequisites as []) ?? []}
       />
-      <Button variant="outline" className="flex gap-2" asChild>
-        <Link href="/">
-          <HiArrowRightOnRectangle />
-          <p>Sign in to enroll</p>
-        </Link>
-      </Button>
+      {!session && (
+        <Button variant="outline" className="flex gap-2" asChild>
+          <Link href="/login">
+            <HiArrowLeftOnRectangle />
+            <p>Sign in to enroll</p>
+          </Link>
+        </Button>
+      )}
+      {!!session && (
+        <Button className="flex gap-2">
+          <HiArrowLeftOnRectangle />
+          <p>Enroll to this course</p>
+        </Button>
+      )}
     </Container>
   );
 }
