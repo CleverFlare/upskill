8e412f5b2ba90d97ac7bc910e52f23b996908c11
Flare <blastclever@gmail.com>
2024-04-03 22:56:49 +0200

    feat(course workspace): add admin panel button

--- src/app/workspace/(layout-group)/_components/sidebar/sidebar-items.tsx
+++ src/app/workspace/(layout-group)/_components/sidebar/sidebar-items.tsx
@@ -1,12 +1,16 @@
 "use client";
 import { Progress } from "@/components/ui/progress";
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
 import tabs from "../tabs";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { HiArrowRightOnRectangle, HiArrowUturnLeft } from "react-icons/hi2";
+import {
+  HiArrowRightOnRectangle,
+  HiArrowUturnLeft,
+  HiOutlineWindow,
+} from "react-icons/hi2";
 import { LuLoader2 } from "react-icons/lu";
 import Logo from "@/components/logo";
 import TabButton from "./tab-button";
@@ -29,12 +33,16 @@ export default function SidebarItems() {
     .replace(/^\/+|\/$/g, "")
     .split("/");
 
-  const isAdmin = pathArray[1] === "admin";
+  const { data: session } = useSession();
+
+  const isAdminPath = pathArray[1] === "admin";
+
+  const isAdmin = session?.user.role === "admin";
 
   return (
     <>
       <Logo />
-      {!isAdmin && (
+      {!isAdminPath && (
         <div className="flex flex-col gap-2">
           <p className="text-xs font-bold uppercase text-gray-700 dark:text-gray-400">
             progress
@@ -51,13 +59,17 @@ export default function SidebarItems() {
         ))}
       </div>
       <div className="mt-auto flex w-full flex-col gap-3">
-        <Button
-          variant="outline"
-          className="flex w-full gap-2 capitalize"
-          asChild
-        >
+        {!isAdminPath && isAdmin && (
+          <Button variant="outline" className="w-full capitalize" asChild>
+            <Link href="/workspace/admin">
+              <HiOutlineWindow className="me-2" />
+              Admin Panel
+            </Link>
+          </Button>
+        )}
+        <Button variant="outline" className="w-full capitalize" asChild>
           <Link href="/">
-            <HiArrowUturnLeft />
+            <HiArrowUturnLeft className="me-2" />
             Back to website
           </Link>
         </Button>
