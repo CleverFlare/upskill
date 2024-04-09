27deff71c6a50e90a46ae7fa9d02d5a8d555c611
Flare <blastclever@gmail.com>
2024-04-09 03:39:39 +0200

    chore: use getToken instead of withAuth HOF in auth middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,36 +1,39 @@
-import { type NextRequestWithAuth, withAuth } from "next-auth/middleware";
-import { type NextFetchEvent, NextResponse } from "next/server";
+import { env } from "@/env";
+import { getToken } from "next-auth/jwt";
+import { NextResponse, type NextRequest } from "next/server";
 
-export default (req: NextRequestWithAuth, _next: NextFetchEvent) =>
-  withAuth(async function middleware({
-    url,
-    nextUrl,
-    nextauth: { token },
-  }: NextRequestWithAuth) {
-    const { origin } = new URL(url);
-    // admin panel logic
-    const isAdmin = token?.user.role === "admin";
-    const isAdminPanel = nextUrl.pathname.startsWith("/workspace/admin");
-    if (!isAdmin && isAdminPanel)
-      return NextResponse.rewrite(new URL("/not-found", url));
+export default async function middleware(request: NextRequest) {
+  const url = new URL(request.url);
+  const session = await getToken({ req: request, secret: env.NEXTAUTH_SECRET });
 
-    // handle unregistered course access
-    const isWorkspaceCourseRoute = /\/workspace\/+/g.test(nextUrl.pathname);
-    if (!isWorkspaceCourseRoute || isAdmin) return NextResponse.next();
-    const courseId = nextUrl.pathname.replace(/^\/|\/$/g, "").split("/");
-    const hasCourseReqData = {
-      courseId,
-      userId: token?.user.id,
-    };
+  if (!session?.user)
+    return NextResponse.redirect(new URL("/login", request.url));
 
-    const hasCourseRes = await fetch(origin + "/api/has-course", {
-      method: "POST",
-      body: JSON.stringify(hasCourseReqData),
-    });
+  // admin panel logic
+  const isAdmin = session?.user.role === "admin";
+  const isAdminPanel = request.nextUrl.pathname.startsWith("/workspace/admin");
+  if (!isAdmin && isAdminPanel)
+    return NextResponse.rewrite(new URL("/not-found", url));
 
-    const hasCourse = hasCourseRes.ok;
+  // handle unregistered course access
+  const isWorkspaceCourseRoute = /\/workspace\/+/g.test(
+    request.nextUrl.pathname,
+  );
+  if (!isWorkspaceCourseRoute || isAdmin) return NextResponse.next();
+  const courseId = request.nextUrl.pathname.replace(/^\/|\/$/g, "").split("/");
+  const hasCourseReqData = {
+    courseId,
+    userId: session?.user.id,
+  };
 
-    if (!hasCourse) return NextResponse.redirect(new URL("/not-found", url));
-  })(req, _next);
+  const hasCourseRes = await fetch(origin + "/api/has-course", {
+    method: "POST",
+    body: JSON.stringify(hasCourseReqData),
+  });
+
+  const hasCourse = hasCourseRes.ok;
+
+  if (!hasCourse) return NextResponse.redirect(new URL("/not-found", url));
+}
 
 export const matcher = "/(workspace|workspace/+)";
