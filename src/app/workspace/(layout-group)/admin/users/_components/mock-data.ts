58b9168cdf8048cf386c513ac92bcee773100bd1
Flare <blastclever@gmail.com>
2024-04-19 23:48:17 +0200

    fix: comment out admin users mock data

--- src/app/workspace/(layout-group)/admin/users/_components/mock-data.ts
+++ src/app/workspace/(layout-group)/admin/users/_components/mock-data.ts
@@ -1,12 +1,12 @@
-import { type Request } from "./columns";
-
-export const data: Request[] = [
-  {
-    id: "1",
-    firstName: "Muhammad",
-    lastName: "Maher",
-    avatar: "/avatars/avatar.jpg",
-    username: "flare",
-    requestDate: "10/1/2021",
-  },
-];
+// import { type User } from "./columns";
+//
+// export const data: User[] = [
+//   {
+//     id: "1",
+//     firstName: "Muhammad",
+//     lastName: "Maher",
+//     avatar: "/avatars/avatar.jpg",
+//     username: "flare",
+//     requestDate: "10/1/2021",
+//   },
+// ];
