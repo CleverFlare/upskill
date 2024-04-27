467495994f7eecc387b97a3b65ebfa35a7d2dd45
Flare <blastclever@gmail.com>
2024-04-27 22:07:35 +0300

    chore: remove topbar search

--- src/app/workspace/(layout-group)/_components/topbar.tsx
+++ src/app/workspace/(layout-group)/_components/topbar.tsx
@@ -11,10 +11,12 @@ export default async function Topbar() {
     <div className="col-end-3 row-start-1 flex w-full items-center gap-4 px-4 py-4">
       <div className="flex flex-1 items-center gap-4">
         <SidebarDrawerButton />
-        <form className="relative w-full max-w-[300px]">
-          <HiMagnifyingGlass className="pointer-events-none absolute left-3 top-1/2 -translate-y-1/2 text-gray-500" />
-          <Input placeholder="Search..." className="w-full ps-8" />
-        </form>
+        {
+          // <form className="relative w-full max-w-[300px]">
+          //   <HiMagnifyingGlass className="pointer-events-none absolute left-3 top-1/2 -translate-y-1/2 text-gray-500" />
+          //   <Input placeholder="Search..." className="w-full ps-8" />
+          // </form>
+        }
       </div>
       <EclipseButton />
       <Avatar className="h-9 w-9">
