de4c7393322a7fa588b95447c185f16f87208732
Flare <blastclever@gmail.com>
2024-04-27 22:40:26 +0300

    feat: add empty state and pagination for the admin courses tab

--- src/app/workspace/(layout-group)/admin/page.tsx
+++ src/app/workspace/(layout-group)/admin/page.tsx
@@ -4,11 +4,15 @@ import { HiPlus } from "react-icons/hi2";
 import Link from "next/link";
 import CourseCardWithActions from "./_components/course-card-with-options";
 import { db } from "@/server/db";
+import Paginator from "@/components/pagination";
 
 export default async function Page({
   searchParams,
 }: {
-  searchParams: Record<string, string | string[]> & { search: string };
+  searchParams: Record<string, string | string[]> & {
+    search: string;
+    page?: string;
+  };
 }) {
   const databaseCoursesData = await db.course.findMany({
     where: {
@@ -26,10 +30,16 @@ export default async function Page({
         },
       ],
     },
+    take: 10,
+    skip: searchParams?.page ? 10 * (+searchParams.page - 1) : 0,
   });
 
+  let coursesCount = await db.course.count({});
+
+  coursesCount = Math.ceil(coursesCount / 10);
+
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex min-h-full flex-col gap-4">
       <h2 className="text-4xl font-bold">Courses</h2>
       <p className="text-gray-500">
         Here you can manage all the courses on the platform.
@@ -43,7 +53,17 @@ export default async function Page({
           </Link>
         </Button>
       </div>
-      <div className="grid grid-cols-1 gap-3 gap-y-8 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+      <div className="grid flex-1 grid-cols-1 gap-3 gap-y-8 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+        {!databaseCoursesData.length && (
+          <div className="col-span-full row-span-full flex h-full flex-1 flex-col items-center justify-center gap-1 rounded-lg border border-border">
+            <p className="text-xl font-bold capitalize">
+              Oh no! No courses available
+            </p>
+            <p className="max-w-[500px] text-center text-muted-foreground">
+              There are no courses to view
+            </p>
+          </div>
+        )}
         {!!databaseCoursesData.length &&
           databaseCoursesData.map((course) => (
             <CourseCardWithActions
@@ -56,6 +76,7 @@ export default async function Page({
             </CourseCardWithActions>
           ))}
       </div>
+      {coursesCount > 1 && <Paginator total={coursesCount} />}
     </div>
   );
 }
